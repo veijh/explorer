ee6e2f69b9a01e0b67a9b660ce830fce8be991c3
veijh <844088636@qq.com>
2024-12-13 08:40:33 +0000

    rename node

--- src/voronoi3D_test.cpp
+++ src/voronoi3D_test.cpp
@@ -263,7 +263,7 @@ void create_stair(
 }
 
 int main(int argc, char *argv[]) {
-  ros::init(argc, argv, "voronoi_test");
+  ros::init(argc, argv, "voronoi3D_test");
   ros::NodeHandle nh("");
 
   ros::Rate rate(1);
